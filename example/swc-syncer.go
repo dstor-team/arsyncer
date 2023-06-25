386c2ea0c30f4328b8574b213de0b5260f5466fe
dstor-team <137670191+dstor-team@users.noreply.github.com>
2023-06-25 04:26:49 -0400

    matched imports to repo

---
+++ example/swc-syncer.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/dstor-team/arsyncer"
+	"github.com/everFinance/goar/types"
+)
+
+// only sync smart contract txs
+func main() {
+	swcFilterParams := arsyncer.FilterParams{
+		Tags: []types.Tag{
+			{Name: "App-Name", Value: "SmartWeaveAction"}, // smart contract tag
+		},
+	}
+	startHeight := int64(472810)
+	arNode := "https://arweave.net"
+	concurrencyNumber := 50 // runtime concurrency number, default 10
+	s := arsyncer.New(startHeight, swcFilterParams, arNode, concurrencyNumber, 15, "subscribe_tx")
+
+	// run
+	s.Run()
+
+	// subscribe tx
+	for {
+		select {
+		case sTx := <-s.SubscribeTxCh():
+			// process synced txs
+			fmt.Println(sTx)
+		}
+	}
+}
